9ed948ca4bbcd70134fab370604000b97a5522c9
anyemaciasr <amaciasr@unicesar.edu.co>
2022-11-10 13:16:53 -0500

    first commit

---
+++ main.tf
@@ -0,0 +1,36 @@
+provider "aws" {
+  region     = "us-east-1"
+  access_key = ""
+  secret_key = ""
+}
+
+
+data "aws_ami" "ubuntu" {
+    most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] 
+}
+
+resource "aws_instance" "web" {
+  ami = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+  key_name = "clave"
+
+  tags = {
+    Name = "web-server"
+  }
+}
+
+output "public_ip" {
+  value       = aws_instance.web.public_ip
+}
